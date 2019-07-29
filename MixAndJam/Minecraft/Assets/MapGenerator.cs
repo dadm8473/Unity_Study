c015b1b7dbcad1b9c647973014b0bdb74ea9c982
dadm8473 <dadm8473@gmail.com>
2019-07-29 11:01:29 +0900

    MineCraft

--- MixAndJam/Minecraft/Assets/MapGenerator.cs
+++ MixAndJam/Minecraft/Assets/MapGenerator.cs
@@ -26,6 +26,7 @@ public class MapGenerator : MonoBehaviour {
     public GameObject B_GrassPrefab;
     public GameObject B_SandPrefab;
     public GameObject B_SnowPrefab;
+    public float blockSetValue;
     
     [Header("[Map Info]")]
     static public int Width_x = 125;
@@ -34,6 +35,7 @@ public class MapGenerator : MonoBehaviour {
     public float GroundHeightOffset = 20;
     public float Wavelength = 0;    // 진폭
     public float Amplitude = 0;     // 파장의 최대 높이
+
 
     public Block[,,] worldBlock = new Block[Width_x, height, Width_z];  // 
     //private List<GameObject> BlockList = new List<GameObject>();
@@ -47,6 +49,97 @@ public class MapGenerator : MonoBehaviour {
 
 	void Update ()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * 100f, Color.red);
+
+            if(Physics.Raycast(ray, out hit, 1000.0f))
+            {
+                Vector3 blockPos = hit.transform.position;
+
+                if (blockPos.y <= 0) return;
+
+                worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z] = null;
+                Destroy(hit.collider.gameObject);
+
+                for (int x = -1; x <= 1; ++x)
+                    for (int y = -1; y <= 1; ++y)
+                        for (int z = -1; z <= 1; ++z)
+                        {
+                            if ((!(x == 0 && y == 0 && z == 0)))
+                            {
+                                if (blockPos.x + x < 0 || blockPos.x + x > Width_x) continue;
+                                if (blockPos.y + y < 0 || blockPos.y + y > height) continue;
+                                if (blockPos.y + z < 0 || blockPos.z + z > Width_z) continue;
+
+                                Vector3 neighbour = new Vector3(blockPos.x + x, blockPos.y + y, blockPos.z + z);
+                                DrawBlock(neighbour);
+                            }
+                        }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * 100f, Color.red);
+
+            if (Physics.Raycast(ray, out hit, 1000.0f))
+            {
+                Vector3 setBlockPos = (Camera.main.transform.position - hit.transform.position).normalized;
+
+                Debug.Log(setBlockPos.y);
+
+                if(setBlockPos.x > 0)
+                    setBlockPos.x = setBlockPos.x < blockSetValue ? 0 : 1;
+                else
+                    setBlockPos.x = setBlockPos.x > -blockSetValue ? 0 : -1;
+
+                if (setBlockPos.y > 0)
+                    setBlockPos.y = setBlockPos.y < blockSetValue ? 0 : 1;
+                else
+                    setBlockPos.y = setBlockPos.y > -blockSetValue ? 0 : -1;
+
+                if (setBlockPos.z > 0)
+                    setBlockPos.z = setBlockPos.z < blockSetValue ? 0 : 1;
+                else
+                    setBlockPos.z = setBlockPos.z > -blockSetValue ? 0 : -1;
+
+                Vector3 blockPos = hit.transform.position + setBlockPos;
+
+                GameObject BlockObj = (GameObject)Instantiate(B_GrassPrefab, blockPos, Quaternion.identity);
+                worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z] = new Block(2, true, BlockObj);
+                Debug.Log("블럭 설치");
+
+                //if (blockPos.y <= 0) return;
+
+                //worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z] = null;
+                //Destroy(hit.collider.gameObject);
+
+                //for (int x = -1; x <= 1; ++x)
+                //    for (int y = -1; y <= 1; ++y)
+                //        for (int z = -1; z <= 1; ++z)
+                //        {
+                //            if ((!(x == 0 && y == 0 && z == 0)))
+                //            {
+                //                if (blockPos.x + x < 0 || blockPos.x + x > Width_x) continue;
+                //                if (blockPos.y + y < 0 || blockPos.y + y > height) continue;
+                //                if (blockPos.y + z < 0 || blockPos.z + z > Width_z) continue;
+
+                //                Vector3 neighbour = new Vector3(blockPos.x + x, blockPos.y + y, blockPos.z + z);
+                //                DrawBlock(neighbour);
+                //            }
+                //        }
+            }
+        }
+
         //if (!Istest)
         //    return;
 
@@ -63,10 +156,15 @@ public class MapGenerator : MonoBehaviour {
     {
         // 맵 만들기
         yield return StartCoroutine(MapInit());
+        Debug.Log("맵 생성");
 
         // 구름 만들기
-        
+        yield return StartCoroutine(CreateCloud(120, 50, 80));
+        Debug.Log("구름 생성");
+
         // 동굴 만들기
+        yield return StartCoroutine(CreateMines(5, 400));
+        Debug.Log("동굴 생성");
     }
 
     private float seed;
@@ -183,6 +281,139 @@ public class MapGenerator : MonoBehaviour {
             else
             {
                 worldBlock[(int)blockpos.x, (int)blockpos.y, (int)blockpos.z] = new Block(5, visual, null);
+            }
+        }
+
+        yield return null;
+    }
+
+    // 땅을 팠을 때 주변 블럭을 생성
+    private void DrawBlock(Vector3 blockPos)
+    {
+        if (worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z] == null) return;
+
+        if(!worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z].vis)
+        {
+            GameObject newBlock = null;
+
+            worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z].vis = true;
+
+            if (worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z].type == 1)
+                newBlock = (GameObject)Instantiate(B_SnowPrefab, blockPos, Quaternion.identity);
+            else if (worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z].type == 2)
+                newBlock = (GameObject)Instantiate(B_GrassPrefab, blockPos, Quaternion.identity);
+            else if (worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z].type == 3)
+                newBlock = (GameObject)Instantiate(B_SandPrefab, blockPos, Quaternion.identity);
+            else if (worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z].type == 4)
+                newBlock = (GameObject)Instantiate(B_GoldPrefab, blockPos, Quaternion.identity);
+            else if (worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z].type == 5)
+                newBlock = (GameObject)Instantiate(B_BedPrefab, blockPos, Quaternion.identity);
+            else
+                worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z].vis = false;
+
+            if (newBlock != null)
+                worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z].obj = newBlock;
+        }
+    }
+
+    IEnumerator CreateMines(int Num, int UnitMineSize)
+    {
+        int HoleSize = 1;
+
+        for (int i = 0; i < Num; ++i)
+        {
+            int xpos = Random.Range(HoleSize, Width_x - HoleSize);
+            int ypos = Random.Range(HoleSize, 15);
+            int zpos = Random.Range(HoleSize, Width_z - HoleSize);
+
+            for (int j = 0; j < UnitMineSize; ++j)
+            {
+                for (int x = -HoleSize; x <= HoleSize; ++x)
+                    for (int y = -HoleSize; y <= HoleSize; ++y)
+                        for (int z = -HoleSize; z <= HoleSize; ++z)
+                        {
+                            Vector3 blockPos = new Vector3(xpos + x, ypos + y, zpos + z);
+
+                            if (worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z] != null)
+                            {
+                                if(worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z].type == 5 ||
+                                   worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z].type == 4)
+                                {
+                                    // 현재 타입이 골드이거나 베드락이면 뚫지 않는다.
+                                    continue;
+                                }
+                                else
+                                {
+                                    Destroy(worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z].obj);
+                                    worldBlock[(int)blockPos.x, (int)blockPos.y, (int)blockPos.z] = null;
+                                }
+                            }
+                        }
+
+                while(true)
+                {
+                    xpos += Random.Range(-1, 2);
+                    if (xpos < HoleSize || xpos >= Width_x - HoleSize) continue;
+                    else break;
+                }
+                while(true)
+                {
+                    zpos += Random.Range(-1, 2);
+                    if (zpos < HoleSize || zpos >= Width_z - HoleSize) continue;
+                    else break;
+                }
+                while(true)
+                {
+                    ypos += Random.Range(-1, 2);
+                    if (ypos < HoleSize || ypos >= height - HoleSize) continue;
+                    else break;
+                }
+            }
+
+            for(int z = 1; z < Width_z - 1; ++z)
+            {
+                for(int x = 1; x < Width_x - 1; ++x)
+                {
+                    for(int y = 1; y < height - 1; ++y)
+                    {
+                        if(worldBlock[x, y, z] == null)
+                        {
+                            for(int x1 = -1; x1 <= 1; ++x1)
+                            {
+                                for(int y1= -1; y1 <= 1; ++y1)
+                                {
+                                    for(int z1 = -1; z1 <= 1; ++z1)
+                                    {
+                                        if(!(x1 == 0 && y1 == 0 && z1 == 0))
+                                        {
+                                            Vector3 neighbour = new Vector3(x + x1, y + y1, z + z1);
+                                            DrawBlock(neighbour);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        yield return null;
+    }
+
+    IEnumerator CreateCloud(int Num, int unitCloudSize, int Cloudbeight)
+    {
+        for(int  i = 0; i < Num; ++i)
+        {
+            int xpos = Random.Range(0, Width_x + 200);
+            int zpos = Random.Range(0, Width_z + 200);
+
+            for(int j = 0; j < unitCloudSize; ++j)
+            {
+                Vector3 blockPos = new Vector3(xpos, Cloudbeight, zpos);
+                Instantiate(B_SnowPrefab, blockPos, Quaternion.identity);
+                xpos += Random.Range(-1, 2);
+                zpos += Random.Range(-1, 2);
             }
         }
 
